8c7e548a4a4fe8227b5fcdefc99534f48d44996c
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-03-25 15:04:44 +0200

    ._.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const usersServise = new UserService('https://jsonplaceholder.typicode.com/users');
+const usersServise = new UserService('https://jsonplaceholder.typicode.com/users', 'https://api.lorem.space/image/face?w=150&h=150');
 
 async function usersApp() { 
     const users = await usersServise.requestAllUsers();
     usersServise.renderAllUsers(users);
 }
-
+ 
 usersApp(); 
--- src/users.js
+++ src/users.js
@@ -1,9 +1,10 @@
-function UserService(url) { 
+function UserService(url, imageUrl) { 
     this.url = url; 
+    this.baseImageUrl = new URL(imageUrl);
 } 
 
 UserService.prototype.requestAllUsers = async function() {
-    try {
+    try { 
         const users = await fetch(this.url);
         return users.json();
     } catch(error) {
@@ -15,22 +16,47 @@ UserService.prototype.requestAllUsers = async function() {
 UserService.prototype.requestUserById = async function(id) {
     try {
         const selectedUser = await fetch(`${this.url}/${id}`);
-        console.log(`${this.url}/${id}`)
         return selectedUser.json();
     } catch(error) {
         console.log(error);
         return {};
     } 
-    // or
-    // try {
-    //     const users = await this.requestAllUsers();
-    //     const selectedUser = users.find(user => { return user.id === id; });
-    //     return selectedUser;
-    // } catch(error) {
-    //     console.log(error);
-    //     return {};
-    // }
 }
+
+UserService.prototype.showSelectedUser = async function(user) {
+    const selectedUserBlock = document.querySelector('.selected-user-block');
+    selectedUserBlock.style.display = 'block';
+    const closeBtn = document.querySelector('.close-btn');
+    closeBtn.addEventListener('click', () => {
+        selectedUserBlock.style.display = 'none';
+    })
+
+    const loader = document.querySelector('.loader');
+
+    loader.style.visibility = 'visible';
+    const selectedUser = await this.requestUserById(user.id);
+    setTimeout(() => {
+        loader.style.visibility = 'hidden';
+    }, 500);
+
+    this.baseImageUrl.searchParams.append('r', selectedUser.id);
+
+    const selectedUserItem = document.querySelector('.selected-user-item')
+    selectedUserItem.querySelector('#user-photo').src = this.baseImageUrl;
+    selectedUserItem.querySelector('#user-name').innerText = selectedUser.name;
+    selectedUserItem.querySelector('#user-username').innerText = `Username: ${selectedUser.username}`;
+
+    const moreInfoArea = document.querySelector('.more-info-area')
+    moreInfoArea.querySelector('#user-company-name').innerText = selectedUser.company.name;
+    moreInfoArea.querySelector('#user-phone').innerText = selectedUser.phone;
+    moreInfoArea.querySelector('#user-email').innerText = selectedUser.email;
+    moreInfoArea.querySelector('#user-address').innerHTML = `
+        ${selectedUser.address.suite},
+        ${selectedUser.address.street},
+        <br>
+        ${selectedUser.address.city} / ${user.address.zipcode}
+    `;
+};
 
 UserService.prototype.renderAllUsers = function(users) {
     const usersList = document.querySelector('.users-list');
@@ -39,68 +65,18 @@ UserService.prototype.renderAllUsers = function(users) {
         const userItem = document.createElement('li');
         userItem.classList.add('user-item');
 
-        const baseImageUrl = new URL("https://api.lorem.space/image/face?w=150&h=150");
-        baseImageUrl.searchParams.append('r', user.id);
+        this.baseImageUrl.searchParams.append('r', user.id);
 
         userItem.innerHTML = `
             <figure>
-                <img src="${baseImageUrl}" alt=""></img>
+                <img src="${this.baseImageUrl}" alt=""></img>
                 <figcaption>
                     <span>${user.name}</span>
                     <span>${user.company.name}</span>
                 </figcaption>
             </figure>
         `;
-
-        const showSelectedUser = async () => {
-            const selectedUserBlock = document.querySelector('.selected-user-block');
-            selectedUserBlock.style.display = 'block';
-            const closeBtn = document.querySelector('.close-btn');
-            closeBtn.addEventListener('click', () => {
-                selectedUserBlock.style.display = 'none';
-            })
-
-            const loader = document.querySelector('.loader');
-            const selectedUserItem = document.querySelector('.selected-user-item');
-            const moreInfoArea = document.querySelector('.more-info-area');
-
-            loader.style.visibility = 'visible';
-            const selectedUser = await this.requestUserById(user.id);
-            setTimeout(() => {
-                loader.style.visibility = 'hidden';
-            }, 500);
-
-            selectedUserItem.innerHTML = userItem.innerHTML;
-            selectedUserItem.querySelector('span:last-of-type').innerText = `Username:  ${selectedUser.username}`;
-
-            moreInfoArea.innerHTML = `
-                <ul>
-                    <li>
-                        <span>Company</span>
-                        <span>${selectedUser.company.name}</span>
-                    </li>
-                    <li>
-                        <span>Phone</span>
-                        <span>${selectedUser.phone}</span>
-                    </li>
-                    <li>
-                        <span>Email</span>
-                        <span><a href='#'>${selectedUser.email}</a></span>
-                    </li>
-                    <li>
-                        <span>Address</span>
-                        <span>
-                        ${selectedUser.address.suite},
-                        ${selectedUser.address.street},
-                        <br>
-                        ${selectedUser.address.city} / ${selectedUser.address.zipcode}
-                        </span>
-                    </li>
-                </ul>
-            `;
-        };
-        
-        userItem.addEventListener('click', showSelectedUser);
+        userItem.addEventListener('click', () => this.showSelectedUser(user));
         usersList.append(userItem);
     });
 }
